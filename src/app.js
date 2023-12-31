4ca733fd8d957673ef3f7284c41f58023f156f96
Jasmin Inneh <110208655+JasminInneh@users.noreply.github.com>
2023-12-31 04:06:38 +0000

    random card generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".card").classList.add("heart");
+  setTimeout(() => {
+    document.querySelector(".card").classList.remove("heart");
+    document.querySelector(".card").classList.add("spade");
+  }, 1000);
+};
+
+let generateRandomNumber(){
+  let num = Math.floor(Math.random() * 12);
+  let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let suit = ["Diamonds", "Spades", "Hearts", "Clubs"];
 };
